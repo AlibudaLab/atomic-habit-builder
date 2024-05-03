b95b36d859bb16ca034f04920f8c76cecf1414a6
ryanycw <ryan@ryanycw.dev>
2024-05-04 02:29:56 +1000

    feat: on ramp integration

--- web/app/habit/components/step2stake.tsx
+++ web/app/habit/components/step2stake.tsx
@@ -1,16 +1,63 @@
 'use client';
 
 import Image from 'next/image';
-import { SetStateAction } from 'react';
+import { useRef, useState, SetStateAction } from 'react';
 import { useAccount, useBalance } from 'wagmi';
+import crypto from "crypto";
+import { GateFiDisplayModeEnum, GateFiSDK, GateFiLangEnum } from "@gatefi/js-sdk";
+
+import { redirect } from "next/navigation";
 
 export default function Step2DepositAndStake({setSteps}: {setSteps: React.Dispatch<SetStateAction<number>>}) {
+  const overlayInstanceSDK = useRef<GateFiSDK | null>(null);
+  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
 
-  const {address} = useAccount()
+  const { address } = useAccount()
 
   const balance = useBalance({address})
 
-  const hasEnoughBalance = false
+  const hasEnoughBalance = (balance.data ?? { value: 0 }).value > 0.01;
+
+  console.log(`address: `, address, `balance: `, balance.data, `hasEnoughBalance: `, hasEnoughBalance)
+
+  const handleOnClick = () => {
+    if (overlayInstanceSDK.current) {
+      if (isOverlayVisible) {
+        console.log('is visible')
+        overlayInstanceSDK.current.hide();
+        setIsOverlayVisible(false);
+      } else {
+        console.log('is not visible')
+        overlayInstanceSDK.current.show();
+        setIsOverlayVisible(true);
+      }
+    } else {
+      const randomString = crypto.randomBytes(32).toString("hex");
+      overlayInstanceSDK.current = new GateFiSDK({
+        merchantId: `${process.env.NEXT_PUBLIC_UNLIMIT_MERCHANTID}`,
+        displayMode: GateFiDisplayModeEnum.Overlay,
+        nodeSelector: "#overlay-button",
+        lang: GateFiLangEnum.en_US,
+        isSandbox: true,
+        successUrl:"https://www.crypto.unlimit.com/",
+        walletAddress: address,
+        externalId: randomString,
+        defaultFiat: {
+          currency: "USD",
+          amount: "20",
+        },
+        defaultCrypto: {
+          currency: "ETH",
+        },
+      });
+    }
+    overlayInstanceSDK.current?.show();
+    setIsOverlayVisible(true);
+  };
+
+  if (!address) {
+    redirect('/')
+  }
 
   return (
     <div className='flex flex-col items-center justify-center'>
@@ -39,12 +86,12 @@ export default function Step2DepositAndStake({setSteps}: {setSteps: React.Dispat
     : <button
         type="button"
         className="rounded-lg bg-yellow-500 mt-4 px-6 py-3 font-bold text-white hover:bg-yellow-600"
-        onClick={() => {
-          console.log('Ryan please onramp')
-        }}
+        onClick={(handleOnClick)}
       >
         Onramp
-      </button>}
+      </button>
+      }
+      <div id="overlay-button"> </div>
     </div>
   );
 }
