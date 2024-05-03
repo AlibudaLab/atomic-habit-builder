9d3790d4fd265a2d6270279fa1da0c66111f77dc
Anton Cheng <antonassocareer@gmail.com>
2024-05-04 00:28:12 +1000

    fix: build bugs

--- web/app/paymaster-bundler/_components/Vault.tsx
+++ web/app/paymaster-bundler/_components/Vault.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { roboto } from 'app/fonts';
+import { nunito } from 'app/fonts';
 import { ALL_ITEMS } from '../constants';
 import { OwnedTokensType } from '../types';
 import Item from './Item';
@@ -11,9 +11,9 @@ type VaultProps = {
 export default function Vault({ ownedTokens }: VaultProps) {
   return (
     <div className="w-full rounded-bl-xl bg-boat-color-gray-900 px-8 py-10">
-      <h2 className={clsx('mb-5 text-2xl', roboto.className)}>VAULT</h2>
+      <h2 className={clsx('mb-5 text-2xl', nunito.className)}>VAULT</h2>
 
-      <h3 className={clsx('mb-1 text-lg', roboto.className)}>Items</h3>
+      <h3 className={clsx('mb-1 text-lg', nunito.className)}>Items</h3>
       <div className="border border-x-0 border-t-0 border-boat-gold" />
 
       <div className="my-4 flex gap-4">
@@ -30,7 +30,7 @@ export default function Vault({ ownedTokens }: VaultProps) {
         })}
       </div>
 
-      <h3 className={clsx('mb-1 text-lg', roboto.className)}>Mystery boxes</h3>
+      <h3 className={clsx('mb-1 text-lg', nunito.className)}>Mystery boxes</h3>
       <div className="border border-x-0 border-t-0 border-boat-gold" />
 
       <div className="my-4 flex gap-4">
