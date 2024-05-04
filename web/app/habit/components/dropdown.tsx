5050c785009c4e5ba7408878b480af477f79abdb
Anton Cheng <antonassocareer@gmail.com>
2024-05-04 23:59:55 +1000

    ui: dropdown

--- web/app/habit/components/dropdown.tsx
+++ web/app/habit/components/dropdown.tsx
@@ -3,32 +3,42 @@ import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { clsx } from 'clsx';
 
 import { Challenge } from '@/hooks/useUserChallenges';
-import { SetStateAction } from 'react';
+import { SetStateAction, useState } from 'react';
 
 import { ActivityTypes, challenges } from '@/constants';
 
 export function ChallengesDropDown({
   setSelectedChallenge,
   selectedChallenge,
+  onChoose,
 }: {
   setSelectedChallenge: React.Dispatch<SetStateAction<Challenge>>;
   selectedChallenge: Challenge;
+  onChoose: (challengeArx: string) => void;
 }) {
+
+  const [open, setOpen] = useState(true)
+
   return (
-    <DropdownMenu.Root>
+    <DropdownMenu.Root >
       <DropdownMenu.Trigger asChild>
         <div className=" flex w-full items-center justify-start gap-6">
-          <button
-            style={{ width: '500px' }}
-            type="button"
-            className="bg-light rounded-md p-2 text-center "
-          >
-            Choose Challenge
+        <div className="flex justify-start rounded-md border-solid border">
+          <div className="p-2 text-2xl">
+            {' '}
+            {selectedChallenge.type === ActivityTypes.Mental ? '💚' : '💪🏻'}{' '}
+          </div>
+          <button onClick={() => setOpen(true)} type="button">
+          <div className="flex flex-col items-start justify-start p-2">
+            <p className="text-xs opacity-80">{selectedChallenge.duration}</p>
+            <p className='text-sm'>{selectedChallenge.name}</p>
+          </div>
           </button>
+        </div>
         </div>
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
-        <DropdownMenu.Content
+        {open && <DropdownMenu.Content
           align="end"
           sideOffset={0}
           className={clsx(
@@ -36,13 +46,17 @@ export function ChallengesDropDown({
             'bg-light rounded-lg border-solid px-6 pb-2 pt-6 shadow',
           )}
         >
-          {challenges.map((challenge) => (
+          {challenges.map((challenge) =>  challenge.arxAddress !== selectedChallenge.arxAddress ? (
             <button
               type="button"
               style={{ borderColor: 'grey' }}
               className="bg-light w-full border-b-2 border-dotted p-2 text-center hover:opacity-80"
               // label={challenge.name}
               key={challenge.arxAddress}
+              onClick={() => {
+                onChoose(challenge.arxAddress)
+                setOpen(false)
+              }}
             >
               <div className="flex justify-start">
                 <div className="p-2 text-2xl">
@@ -50,13 +64,13 @@ export function ChallengesDropDown({
                   {challenge.type === ActivityTypes.Mental ? '💚' : '💪🏻'}{' '}
                 </div>
                 <div className="flex flex-col items-start justify-start p-2">
-                  <p className="text-sm opacity-80">{challenge.duration}</p>
-                  <p>{challenge.name}</p>
+                  <p className="text-xs opacity-80">{challenge.duration}</p>
+                  <p className='text-sm'>{challenge.name}</p>
                 </div>
               </div>
             </button>
-          ))}
-        </DropdownMenu.Content>
+          ) : <> </>)}
+        </DropdownMenu.Content>}
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
   );
