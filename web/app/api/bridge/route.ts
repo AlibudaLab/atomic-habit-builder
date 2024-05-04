b235b8d1ea99bb63c2f3c9bcd6807437b6b488da
ryanycw <ryan@ryanycw.dev>
2024-05-04 14:07:56 +1000

    chore: yarn format

--- web/app/api/bridge/route.ts
+++ web/app/api/bridge/route.ts
@@ -12,22 +12,22 @@ export async function POST(req: NextRequest): Promise<Response> {
   try {
     const payload = await req.json();
     // Get notification
-    if(payload?.event == 'onramp.trade.succeeded') {
+    if (payload?.event == 'onramp.trade.succeeded') {
       // Get the destination wallet
       const destinationWallet = payload.data.destinationWallet;
       // Create account with private key
       const client = createWalletClient({
         chain: baseSepolia,
-        transport: http()
-      })
+        transport: http(),
+      });
       const account = privateKeyToAccount(`0x${process.env.FAUCET_PRIVATE_KEY}`);
       // Airdrop eth to the user
-      const hash = await client.sendTransaction({ 
+      const hash = await client.sendTransaction({
         account,
         chain: baseSepolia,
         to: destinationWallet,
-        value: parseEther(payload.data.cryptoAmount)
-      })
+        value: parseEther(payload.data.cryptoAmount),
+      });
       return NextResponse.json(hash, { status: 200 });
     } else {
       return NextResponse.json({}, { status: 400, statusText: 'Invalid Payload' });
