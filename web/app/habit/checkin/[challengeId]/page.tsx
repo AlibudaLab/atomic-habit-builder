64c3bbb08f6fa10e695ff321492bfa50362ed032
Anton Cheng <antonassocareer@gmail.com>
2024-05-09 13:41:29 +1000

    refactor: use shared header

--- web/app/habit/checkin/[challengeId]/page.tsx
+++ web/app/habit/checkin/[challengeId]/page.tsx
@@ -1,32 +1,16 @@
 'use client';
 
-import { useState } from 'react';
+import CheckIn from '../components/checkin';
 
-import Step3CheckIn from '../components/checkin';
-import { Toaster } from 'react-hot-toast';
-import Image from 'next/image';
-import { Challenge } from '@/hooks/useUserChallenges';
-
-const nouns = require('@/imgs/nouns.png') as string;
-
-import { challenges } from '@/constants';
-import { useSearchParams } from 'next/navigation';
+import Header from '../../components/Header';
 
 export default function CheckInLayout() {
 
   return (
     <main className="container mx-auto flex flex-col items-center px-8 pt-16">
-      <Toaster />
-      <button type="button" onClick={() => {console.log('img clicked')}}>
-        <Image src={nouns} width="100" height="100" alt="Nouns Logo" className="mb-10" />
-      </button>
+      <Header />
 
-      <div className="font-title container mb-10 w-full text-center text-4xl">
-        {' '}
-        Alibuda Habit Builder{' '}
-      </div>
-
-      <Step3CheckIn />
+      <CheckIn />
     </main>
   );
 }
