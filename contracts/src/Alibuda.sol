a6695fe83afffa3e68648b37f4175f057527b7f2
ryanycw <ryan@ryanycw.dev>
2024-05-12 00:46:38 +0800

    build: add max amount

--- contracts/src/Alibuda.sol
+++ contracts/src/Alibuda.sol
@@ -3,12 +3,17 @@ pragma solidity 0.8.25;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+import "./config/errors.sol";
+
 contract Alibuda is ERC20 {
     constructor() ERC20("Alibuda", "ALB") {
-        _mint(msg.sender, 1_000_000_000 * 10 ** 18);
+        _mint(msg.sender, 1_000_000_000 * 1e18);
     }
 
     function mint(address to, uint256 amount) public {
+        if (amount > 1_000 * 1e18) {
+            revert ExceedMaxAmount();
+        }
         _mint(to, amount);
     }
 }
