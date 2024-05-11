79d4ae2d9cadaabbec62f4c5dabd82b359841b86
ryanycw <ryan@ryanycw.dev>
2024-05-11 09:24:39 +0800

    test: adjust test with deposit erc20

--- contracts/test/Tracker.t.sol
+++ contracts/test/Tracker.t.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.25;
 
 import "forge-std/Test.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "../src/Alibuda.sol";
 import "../src/Tracker.sol";
-import "forge-std/console.sol";
 
 contract TrackerTest is Test {
+    Alibuda underlying;
     Tracker tracker;
     /* below are the values retrieved from arx signing */
     address arxAddress = address(0x883167E6b5d489B82cB97bEf9C7967afe3A3D299);
@@ -14,10 +18,12 @@ contract TrackerTest is Test {
     bytes32 s = 0x32f97aa964371f4afa35ce35234b59df65ffbb4260ea71349d374742b012364b;
 
     function setUp() public {
-        tracker = new Tracker();
+        underlying = new Alibuda();
+        tracker = new Tracker(address(underlying));
         tracker.register(
             arxAddress, "test challenge", 1, block.timestamp + 10, block.timestamp + 1000, address(this), 0.0001 ether
         );
+        underlying.approve(address(tracker), 0.0001 ether);
     }
 
     function test_register() public view {
@@ -26,12 +32,12 @@ contract TrackerTest is Test {
     }
 
     function test_join() public {
-        tracker.join{value: 0.0001 ether}(arxAddress);
+        tracker.join(arxAddress);
         require(tracker.hasJoined(arxAddress, address(this)), "Join failed");
     }
 
     function test_checkIn() public {
-        tracker.join{value: 0.0001 ether}(arxAddress);
+        tracker.join(arxAddress);
         bytes memory encoded = abi.encodePacked("\x19Ethereum Signed Message:\n21test check in message");
         bytes32 msgHash = keccak256(encoded);
 
@@ -41,7 +47,7 @@ contract TrackerTest is Test {
     }
 
     function test_settle() public {
-        tracker.join{value: 0.0001 ether}(arxAddress);
+        tracker.join(arxAddress);
         bytes memory encoded = abi.encodePacked("\x19Ethereum Signed Message:\n21test check in message");
         bytes32 msgHash = keccak256(encoded);
 
