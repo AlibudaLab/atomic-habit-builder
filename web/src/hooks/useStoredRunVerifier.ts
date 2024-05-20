38008928015d39de9600ab8fe6876ff3bdfe74aa
Anton Cheng <antonassocareer@gmail.com>
2024-05-20 22:35:14 +0800

    fix: use local-storage-fallback package to avoid relying on local storage

--- web/src/hooks/useStoredRunVerifier.ts
+++ web/src/hooks/useStoredRunVerifier.ts
@@ -3,6 +3,7 @@
 import { useCallback, useEffect, useState } from 'react';
 
 import { RunVerifier } from '@/types';
+import storage from 'local-storage-fallback'
 
 const STORAGE_KEY_VERIFIER = 'run-verifier';
 const STORAGE_KEY_SECRET = 'run-verifier-secret';
@@ -20,9 +21,9 @@ export const useRunVerifier = () => {
     if (!window) return;
 
     const storedVerifier =
-      (window.localStorage.getItem(STORAGE_KEY_VERIFIER) as RunVerifier) ?? RunVerifier.None;
-    const storedSecret = window.localStorage.getItem(STORAGE_KEY_SECRET) ?? null;
-    const storedExpiry = Number(window.localStorage.getItem(STORAGE_KEY_EXPIRY)) ?? 0;
+      (storage.getItem(STORAGE_KEY_VERIFIER) as RunVerifier) ?? RunVerifier.None;
+    const storedSecret = storage.getItem(STORAGE_KEY_SECRET) ?? null;
+    const storedExpiry = Number(storage.getItem(STORAGE_KEY_EXPIRY)) ?? 0;
 
     setVerifier(storedVerifier);
     setSecret(storedSecret);
@@ -32,14 +33,14 @@ export const useRunVerifier = () => {
   const updateVerifierAndSecret = useCallback(
     (newVerifier: RunVerifier, newSecret: string, newExpiry?: number) => {
       setVerifier(newVerifier);
-      localStorage.setItem(STORAGE_KEY_VERIFIER, newVerifier);
+      storage.setItem(STORAGE_KEY_VERIFIER, newVerifier);
 
       setSecret(newSecret);
-      localStorage.setItem(STORAGE_KEY_SECRET, newSecret);
+      storage.setItem(STORAGE_KEY_SECRET, newSecret);
 
       if (newExpiry) {
         setExpiry(newExpiry);
-        localStorage.setItem(STORAGE_KEY_EXPIRY, newExpiry.toString());
+        storage.setItem(STORAGE_KEY_EXPIRY, newExpiry.toString());
       }
     },
     [setVerifier, setSecret, setExpiry],
