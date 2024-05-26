652298ce24a6bb3d7f6cf84fbc3f76f9c26ea990
Anton Cheng <antonassocareer@gmail.com>
2024-05-26 13:55:57 +0800

    chore: addd script

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -6,10 +6,9 @@ import { initAnalytics } from '@/utils/analytics';
 import { roboto, londrina } from './fonts';
 import type { Metadata } from 'next';
 
-// export const viewport = {
-//   width: 'device-width',
-//   initialScale: 1.0,
-// };
+export const headers = {
+
+}
 
 export const metadata: Metadata = {
   manifest: '/manifest.json',
@@ -28,6 +27,7 @@ initAnalytics();
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${roboto.className} ${londrina.variable} ${roboto.variable}`}>
+      <script>  </script>
       <body className="flex flex-1 flex-col">
         <OnchainProviders>
           <div className="lg:mx-auto lg:max-w-3xl lg:px-32">{children}</div>
