8a9b951619b766d7586397a72ce7f1f0eaa61f44
ryanycw <ryan@ryanycw.dev>
2024-06-08 01:31:29 +0800

    build: add new claimed reward popup

---
+++ web/app/habit/claim/components/ClaimedPopup.tsx
@@ -0,0 +1,52 @@
+import { useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+import PopupWindow from '@/components/PopupWindow/PopupWindow';
+
+type ClaimedPopupProps = {
+  onClose: () => void;
+  onCheckInPageClick: () => void;
+};
+
+function ClaimedPopup({ onClose, onCheckInPageClick }: ClaimedPopupProps) {
+  const title = 'Reward Received!';
+
+  const content = (
+    <div className="flex flex-col items-center text-left">
+      <ul className="mb-4 list-disc pl-5">
+        <li>Nouns NFT Claimed</li>
+        <li>Stake Returned</li>
+        <li>Stake Reward Received</li>
+      </ul>
+    </div>
+  );
+
+  const handleShareOnFarcaster = () => {};
+
+  const buttons = useMemo(() => {
+    return [
+      {
+        id: 'shareOnFarcaster',
+        label: 'Share on Farcaster',
+        onClick: handleShareOnFarcaster,
+        className: 'popup-buttons',
+        disabled: true,
+      },
+      {
+        id: 'backToChallengeList',
+        label: 'Back to Challenge List',
+        onClick: onCheckInPageClick,
+        className: 'popup-buttons',
+      },
+    ];
+  }, []);
+
+  return <PopupWindow title={title} onClose={onClose} content={content} buttons={buttons} />;
+}
+
+ClaimedPopup.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  onCheckInPageClick: PropTypes.func.isRequired,
+};
+
+export default ClaimedPopup;
