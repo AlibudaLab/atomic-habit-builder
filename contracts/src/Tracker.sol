d828cfda692fe7a00dd058f83ac8c3617dd31bd1
Anton Cheng <antonassocareer@gmail.com>
2024-06-14 19:19:11 +0300

    feat: natspec, random bugs and view function (#59)

--- contracts/src/Tracker.sol
+++ contracts/src/Tracker.sol
@@ -32,6 +32,7 @@ contract Tracker is EIP712 {
     mapping(uint256 challengeId => address[]) public succeedUsers;
     mapping(bytes32 digest => bool) public digestUsed;
     mapping(address user => uint256[]) public userChallenges;
+
     mapping(uint256 challengeId => mapping(address user => uint256[])) public checkIns;
     mapping(uint256 challengeId => mapping(address user => bool)) public hasJoined;
     mapping(uint256 challengeId => mapping(address user => bool)) public claimable;
@@ -56,8 +57,18 @@ contract Tracker is EIP712 {
         underlyingToken = _underlyingToken;
     }
 
-    //FIXME: This should be created with token specify
-    // register a new habit challenge
+    /**
+     * @notice Register a new challenge
+     * @param verifier verifier address to sign the check in data
+     * @param extraData additional data to emit
+     * @param minimumCheckIns minimum check ins required to succeed
+     * @param startTimestamp start timestamp of the challenge
+     * @param joinDueTimestamp latest timestamp to join the challenge
+     * @param endTimestamp end timestamp of the challenge
+     * @param donateDestination address to donate half of the failed user stake
+     * @param checkInJudge address of the contract to judge the check in data
+     * @param stake stake amount required to join the challenge
+     */
     function register(
         address verifier,
         string memory extraData,
@@ -68,9 +79,10 @@ contract Tracker is EIP712 {
         address donateDestination,
         address checkInJudge,
         uint256 stake
-    ) public {
+    ) external {
         require(endTimestamp > startTimestamp, "end timestamp must be greater than start timestamp");
         require(joinDueTimestamp <= endTimestamp, "should not allow joining time larger than challenge ending time");
+        require(donateDestination != address(0), "invalid donate destination");
         challenges[++challengeCounter] = Challenge(
             verifier,
             minimumCheckIns,
@@ -96,8 +108,11 @@ contract Tracker is EIP712 {
         );
     }
 
-    // user join a habit challenge
-    function join(uint256 challengeId) public payable {
+    /**
+     * @dev Users need to approve the tracker contract to transfer the stake amount before calling this function.
+     * @param challengeId id of the challenge to join
+     */
+    function join(uint256 challengeId) external payable {
         require(challenges[challengeId].startTimestamp != 0, "challenge does not exist");
         require(!hasJoined[challengeId][msg.sender], "user already joined the challenge");
         require(block.timestamp < challenges[challengeId].joinDueTimestamp, "joining period has ended");
@@ -111,17 +126,17 @@ contract Tracker is EIP712 {
     }
 
     /**
+     * @dev If the checkInJudge contract is specified by the challenge, checkInJudge.judge(checkInData) will be called.
+     * checkInJudge.judge(checkInData) should custom checks in addition to tha main tracker checks.
+     * The main tracker is only responsible for checking:
+     * 1. The signature  is from the verifier
+     * 2. The challenge ID is the ID included in the signing digest.
+     * 3. The msg.sender is the user address included in the signing digest.
      * @param challengeId id of the challenge to check in, required to prevent replays from other challenges in case the same verifiers are used.
      * @param checkInData application specific extra check in data.
      * @param signature checkInDigest signed by verifier address.
-     * @dev If the checkInJudge contract is specified, checkInData would be passed with the call checkInJudge.judge(checkInData).
-     * checkInJudge.judge(checkInData) should implement what to check on and replay prevention and the checkInJudge contract address should be specified at challenge creation.
-     * The main tracker is only responsible for checking:
-     * 1. If the signing address matches the validator address
-     * 2. The challenge ID is the ID included in the signing digest.
-     * 3. The msg.sender is the user address included in the signing digest.
      */
-    function checkIn(uint256 challengeId, bytes memory checkInData, bytes memory signature) public {
+    function checkIn(uint256 challengeId, bytes memory checkInData, bytes memory signature) external {
         uint256 timestamp = block.timestamp;
         require(
             timestamp >= challenges[challengeId].startTimestamp && timestamp <= challenges[challengeId].endTimestamp,
@@ -152,8 +167,12 @@ contract Tracker is EIP712 {
         emit CheckIn(msg.sender, challengeId, timestamp);
     }
 
-    // settle a challenge after the ending timestamp
-    function settle(uint256 challengeId) public {
+    /**
+     * @notice Settle the challenge, transfer half of the failed user stake to the donateDestination address
+     * @param challengeId id of the challenge to settle
+     */
+    function settle(uint256 challengeId) external {
+        require(challenges[challengeId].startTimestamp != 0, "challenge does not exist");
         require(block.timestamp > challenges[challengeId].endTimestamp, "challenge has not ended");
         require(!challenges[challengeId].settled, "challenge already settled");
         emit Settle(challengeId);
@@ -175,7 +194,7 @@ contract Tracker is EIP712 {
         return challenges[challengeId].totalStake / succeedUserCount;
     }
 
-    function withdraw(uint256 challengeId) public {
+    function withdraw(uint256 challengeId) external {
         require(challenges[challengeId].settled, "challenge not yet settled");
         uint256 amount = getClaimableAmount(challengeId, msg.sender);
         claimable[challengeId][msg.sender] = false;
@@ -199,11 +218,31 @@ contract Tracker is EIP712 {
         );
     }
 
-    function getUserChallenges(address user) public view returns (uint256[] memory) {
+    /**
+     * @notice Get the list of challenges that the user has joined
+     */
+    function getUserChallenges(address user) external view returns (uint256[] memory) {
         return userChallenges[user];
     }
 
-    function getUserCheckInCounts(uint256 challengeId, address user) public view returns (uint256) {
+    /**
+     * @notice Get the amount of check-ins that the user has done for a challenge
+     */
+    function getUserCheckInCounts(uint256 challengeId, address user) external view returns (uint256) {
         return checkIns[challengeId][user].length;
+    }
+
+    /**
+     * @notice Get the amount of users that have joined a challenge
+     */
+    function getChallengeParticipantsCount(uint256 challengeId) external view returns (uint256) {
+        return users[challengeId].length;
+    }
+
+    /**
+     * @notice Get the amount of users that have succeeded in a challenge
+     */
+    function getChallengeSucceedParticipantsCount(uint256 challengeId) external view returns (uint256) {
+        return succeedUsers[challengeId].length;
     }
 }
--- contracts/src/interfaces/ICheckInJudge.sol
+++ contracts/src/interfaces/ICheckInJudge.sol
@@ -1,6 +1,17 @@
 // SPDX-License_Identifier: MIT
 pragma solidity 0.8.25;
 
+/**
+ * @title ICheckInJudge
+ * @notice This is an optional interface for challenges to use a custom check-in judge.
+ * @dev It should do custom checks in addition to tha main tracker checks. Some examples are:
+ * - Check if the checkInData is already used.
+ */
 interface ICheckInJudge {
+
+    /**
+     * @notice The custom checkin judge should implement this function.
+     * @param checkInData any data that the judge needs to verify the check-in.
+     */
     function judge(bytes memory checkInData) external returns (bool);
 }
