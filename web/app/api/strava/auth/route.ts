b5ee6baf497edcfc98d312b825bf858b2a4b7dfd
ryanycw <ryan@ryanycw.dev>
2024-06-26 21:32:49 +0800

    chore: remove log

--- web/app/api/strava/auth/route.ts
+++ web/app/api/strava/auth/route.ts
@@ -14,8 +14,6 @@ export async function POST(req: NextRequest): Promise<Response> {
     const url = `https://www.strava.com/api/v3/oauth/token?client_id=${process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID}&client_secret=${process.env.STRAVA_CLIENT_SECRET}&code=${authToken}&grant_type=authorization_code`;
     const response = await fetch(url, { method: 'POST' });
 
-    console.log(url);
-    console.log('status: ', response.status);
     const userProfile = await response.json();
     console.log('user profile: ', userProfile);
 
