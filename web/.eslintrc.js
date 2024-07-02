45c447b83e3d4e5128317a2ef3a901c6a08db7f8
Anton Cheng <antonassocareer@gmail.com>
2024-07-02 23:24:53 +0800

    chore: lint rules

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -136,6 +136,7 @@ module.exports = {
 
     // We prefer labels to be associated with inputs
     'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/control-has-associated-label' : 'off',
     'jsx-a11y/label-has-for': [
       'error',
       {
