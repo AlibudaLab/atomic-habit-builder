e3d900707ada6f30d53b038cd2ffe152b0c8bb8f
ryanycw <ryan@ryanycw.dev>
2024-08-13 17:08:01 +0800

    refactor: update deploy script with initGov

--- contracts/script/D01DeployChallenges.s.sol
+++ contracts/script/D01DeployChallenges.s.sol
@@ -6,7 +6,8 @@ import {IChallenges} from "../src/interfaces/IChallenges.sol";
 import {Challenges} from "../src/Challenges.sol";
 
 contract Deploy is Script {
-    address official_verifier = 0x29C3d6b54E2F8Ae641Fc331cF2143B6d05c97897;
+    address initGovernance = 0x29C3d6b54E2F8Ae641Fc331cF2143B6d05c97897;
+    address officialVerifier = 0x29C3d6b54E2F8Ae641Fc331cF2143B6d05c97897;
 
     function setUp() public {}
 
@@ -22,48 +23,48 @@ contract Deploy is Script {
 
     function deploy() public {
         address underlying = vm.envAddress("TESTNET_TOKEN");
-        Challenges challenges = new Challenges("Habit Builder", "1.0");
+        Challenges challenges = new Challenges("Habit Builder", "1.0", initGovernance);
         challenges.create(
             IChallenges.Challenge(
-                official_verifier,
+                officialVerifier,
                 3,
                 1720418400, // July 8
                 1720828800, // July 13 0:00
                 1720828800, // July 13 0:00
-                official_verifier,
-                5_000,
+                officialVerifier,
                 address(0),
                 underlying,
+                5_000,
                 50 * 1e6
             )
         );
 
         challenges.create(
             IChallenges.Challenge(
-                official_verifier,
+                officialVerifier,
                 12,
                 1720396800, // 7/8
                 1723075200, // 8/8
                 1723075200, // 8/8
-                official_verifier,
-                5_000,
+                officialVerifier,
                 address(0),
                 underlying,
+                5_000,
                 50 * 1e6
             )
         );
 
         challenges.create(
             IChallenges.Challenge(
-                official_verifier,
+                officialVerifier,
                 10,
                 1719763200, // July 1
                 1722355200, // July 31
                 1722355200, // July 31
-                official_verifier,
-                5_000,
+                officialVerifier,
                 address(0),
                 underlying,
+                5_000,
                 25 * 1e6
             )
         );
