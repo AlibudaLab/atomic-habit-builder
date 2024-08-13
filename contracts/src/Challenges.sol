043c373fe6dd98dddc290a4f6ef977ad097c99a7
ryanycw.eth <63644531+ryanycw@users.noreply.github.com>
2024-08-14 00:16:09 +0800

    refactor: contract optimize (#98)

---
+++ contracts/src/Challenges.sol
@@ -0,0 +1,254 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import "./interfaces/IChallenges.sol";
+import "./interfaces/ICheckInJudge.sol";
+
+contract Challenges is IChallenges, EIP712, Ownable {
+    using SafeERC20 for IERC20;
+
+    /// @dev Maximum basis points
+    uint128 public constant MAX_BPS = 10_000;
+
+    /// @dev Minimum donation basis points
+    uint128 public minDonationBPS;
+
+    /// @dev Counter for the challenge id, starts from 1
+    uint256 public challengeCounter;
+
+    /// @dev Mapping of challenge id to challenge struct
+    mapping(uint256 challengeId => Challenge) internal challenges;
+
+    /// @dev Mapping of challenge id to total users joined the challenge
+    mapping(uint256 challengeId => uint128) public totalUsers;
+
+    /// @dev Mapping of challenge id to total users succeeded in the challenge
+    mapping(uint256 challengeId => uint128) public totalSucceedUsers;
+
+    /// @dev Mapping of challenge id to the challenge status
+    mapping(uint256 challengeId => ChallengeStatus) public challengeStatus;
+
+    /// @dev Mapping of challenge id to the user status in the challenge
+    mapping(uint256 challengeId => mapping(address user => UserStatus)) public userStatus;
+
+    /// @dev Mapping of check in digest to whether it has been used
+    mapping(bytes32 digest => bool) public digestUsed;
+
+    /// @dev Mapping of challenge id to user check ins data
+    mapping(uint256 challengeId => mapping(address user => bytes[])) public checkIns;
+
+    /// @dev Mapping of donation org to whether it is enabled
+    mapping(address donationOrg => bool) public donationOrgsEnabled;
+
+    modifier challengeExist(uint256 challengeId) {
+        if (challengeStatus[challengeId] == ChallengeStatus.NotExist) revert ChallengeNotExist();
+        _;
+    }
+
+    constructor(string memory name, string memory version, address initGovernance, uint128 initMinDonationBPS)
+        EIP712(name, version)
+        Ownable(initGovernance)
+    {
+        if (initMinDonationBPS > MAX_BPS) revert InvalidBPS();
+        minDonationBPS = initMinDonationBPS;
+    }
+
+    /**
+     * @notice Create a new challenge
+     * @dev Refer to IChallenges.sol for the Challenge struct
+     * @param challenge Challenge struct to create
+     */
+    function create(Challenge memory challenge) external {
+        if (challenge.endTimestamp <= block.timestamp) revert InvalidTimestamp();
+        if (challenge.joinDueTimestamp >= challenge.endTimestamp) revert InvalidTimestamp();
+        if (challenge.startTimestamp >= challenge.joinDueTimestamp) revert InvalidTimestamp();
+        if (challenge.donateDestination == address(0) || challenge.verifier == address(0)) revert ZeroAddress();
+        if (!donationOrgsEnabled[challenge.donateDestination]) revert InvalidPermission();
+        if (challenge.donationBPS > MAX_BPS || challenge.donationBPS < minDonationBPS) revert InvalidBPS();
+
+        uint256 challengeId = ++challengeCounter;
+        challenges[challengeId] = challenge;
+        challengeStatus[challengeId] = ChallengeStatus.Created;
+
+        emit Create(challengeCounter, challenge);
+    }
+
+    /**
+     * @notice Join a challenge
+     * @dev Users need to approve the tracker contract to transfer the stake amount before calling this function
+     * @param challengeId Id of the challenge to join
+     */
+    function join(uint256 challengeId) external challengeExist(challengeId) {
+        if (userStatus[challengeId][msg.sender] != UserStatus.NotExist) revert ChallengeJoined();
+        if (block.timestamp > challenges[challengeId].joinDueTimestamp) revert InvalidTimestamp();
+
+        IERC20(challenges[challengeId].asset).safeTransferFrom(
+            msg.sender, address(this), challenges[challengeId].stakePerUser
+        );
+        userStatus[challengeId][msg.sender] = UserStatus.Joined;
+        totalUsers[challengeId]++;
+
+        emit Join(msg.sender, challengeId);
+    }
+
+    /**
+     * @notice Check in to a challenge
+     * @dev If the checkInJudge contract is specified by the challenge, checkInJudge.judge(checkInData) will be called
+     * checkInJudge.judge(checkInData) should custom checks in addition to tha main tracker checks
+     * The main tracker is only responsible for checking:
+     * 1. The signature  is from the verifier
+     * 2. The challenge ID is the ID included in the signing digest
+     * 3. The msg.sender is the user address included in the signing digest
+     * @param challengeId Id of the challenge to check in
+     * @param checkInData Application specific extra check in data
+     * @param signature CheckInDigest signed by verifier address
+     */
+    function checkIn(uint256 challengeId, bytes memory checkInData, bytes memory signature)
+        external
+        challengeExist(challengeId)
+    {
+        uint256 timestamp = block.timestamp;
+        if (timestamp < challenges[challengeId].startTimestamp) revert InvalidTimestamp();
+        if (timestamp > challenges[challengeId].endTimestamp) revert InvalidTimestamp();
+        if (userStatus[challengeId][msg.sender] != UserStatus.Joined) revert ChallengeNotJoined();
+
+        bytes32 digest = getCheckInDigest(challengeId, checkInData);
+        if (digestUsed[digest]) revert DigestUsed();
+        if (!SignatureChecker.isValidSignatureNow(challenges[challengeId].verifier, digest, signature)) {
+            revert InvalidSignature();
+        }
+
+        address checkInJudge = challenges[challengeId].checkInJudge;
+        if (checkInJudge != address(0) && !ICheckInJudge(checkInJudge).judge(checkInData)) revert CheckInJudgeError();
+
+        digestUsed[digest] = true;
+        checkIns[challengeId][msg.sender].push(checkInData);
+
+        if (checkIns[challengeId][msg.sender].length == challenges[challengeId].minimumCheckIns) {
+            totalSucceedUsers[challengeId]++;
+            userStatus[challengeId][msg.sender] = UserStatus.Claimable;
+        }
+
+        emit CheckIn(msg.sender, challengeId, checkInData);
+    }
+
+    /**
+     * @notice Settle the challenge, transfer half of the failed user stake to the donateDestination address
+     * If no user has finished the challenge, the donation amount will be 100% of the failed user stake
+     * @param challengeId Id of the challenge to settle
+     */
+    function settle(uint256 challengeId) external challengeExist(challengeId) {
+        if (block.timestamp < challenges[challengeId].endTimestamp) revert InvalidTimestamp();
+        if (challengeStatus[challengeId] == ChallengeStatus.Settled) revert ChallengeSettled();
+
+        uint128 succeedUserCount = totalSucceedUsers[challengeId];
+        uint128 donationBPS = succeedUserCount == 0 ? MAX_BPS : challenges[challengeId].donationBPS;
+        uint256 amountToTransfer =
+            (totalUsers[challengeId] - succeedUserCount) * challenges[challengeId].stakePerUser * donationBPS / MAX_BPS;
+
+        IERC20(challenges[challengeId].asset).safeTransfer(challenges[challengeId].donateDestination, amountToTransfer);
+        challengeStatus[challengeId] = ChallengeStatus.Settled;
+
+        emit Settle(challengeId);
+    }
+
+    /**
+     * @notice Withdraw the winning stake
+     * @param challengeId Id of the challenge to withdraw
+     */
+    function withdraw(uint256 challengeId) external challengeExist(challengeId) {
+        if (challengeStatus[challengeId] != ChallengeStatus.Settled) revert ChallengeNotSettled();
+        if (userStatus[challengeId][msg.sender] != UserStatus.Claimable) revert UserNotClaimable();
+
+        uint128 amount = getWinningStakePerUser(challengeId);
+        if (amount == 0) revert ZeroAmount();
+
+        userStatus[challengeId][msg.sender] = UserStatus.Claimed;
+        IERC20(challenges[challengeId].asset).safeTransfer(msg.sender, amount);
+
+        emit Claim(msg.sender, challengeId, amount);
+    }
+
+    /**
+     * @notice Get the check in digest
+     * @param challengeId Id of the challenge to check in
+     * @param checkInData Application specific extra check in data
+     * @return digest Check-in digest to prevent replay attack
+     */
+    function getCheckInDigest(uint256 challengeId, bytes memory checkInData) public view returns (bytes32) {
+        return _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    keccak256("checkInSigningMessage(uint256 challengeId,address user,bytes checkInData)"),
+                    challengeId,
+                    msg.sender,
+                    keccak256(checkInData)
+                )
+            )
+        );
+    }
+
+    /**
+     * @notice Get the challenge details
+     * @param challengeId Id of the challenge to get the details
+     * @return challenge Challenge struct of the challenge
+     */
+    function getChallenge(uint256 challengeId) external view returns (Challenge memory) {
+        return challenges[challengeId];
+    }
+
+    /**
+     * @notice Get the winning stake per user
+     * @param challengeId Id of the challenge to get the winning stake
+     * @return amount Amount of winning stake per user
+     */
+    function getWinningStakePerUser(uint256 challengeId) public view returns (uint128) {
+        uint128 totalUsersCount = totalUsers[challengeId];
+        uint128 succeedUserCount = totalSucceedUsers[challengeId];
+        uint128 stakePerUser = challenges[challengeId].stakePerUser;
+
+        if (challengeStatus[challengeId] != ChallengeStatus.Settled || succeedUserCount == 0) return 0;
+
+        uint128 totalStake = totalUsersCount * stakePerUser;
+        uint128 donation =
+            (totalUsersCount - succeedUserCount) * stakePerUser * challenges[challengeId].donationBPS / MAX_BPS;
+
+        return (totalStake - donation) / succeedUserCount;
+    }
+
+    /**
+     * @notice Get the amount of check-ins that the user has done for a challenge
+     * @param challengeId Id of the challenge
+     * @param user Address of the user
+     * @return checkInCounts Amount of check-ins that the user has done
+     */
+    function getUserCheckInCounts(uint256 challengeId, address user) external view returns (uint256) {
+        return checkIns[challengeId][user].length;
+    }
+
+    /**
+     * @notice Enable or disable the donation org
+     * @param donationOrg Address of the donation org
+     * @param enabled Whether to enable or disable the donation org
+     */
+    function setDonationOrgEnabled(address donationOrg, bool enabled) external onlyOwner {
+        if (donationOrg == address(0)) revert ZeroAddress();
+        donationOrgsEnabled[donationOrg] = enabled;
+        emit DonationOrgSet(donationOrg, enabled);
+    }
+
+    /**
+     * @notice Set the minimum donation basis points
+     * @param _minDonationBPS Minimum donation basis points
+     */
+    function setMinDonationBPS(uint128 _minDonationBPS) external onlyOwner {
+        if (_minDonationBPS > MAX_BPS) revert InvalidBPS();
+        minDonationBPS = _minDonationBPS;
+        emit MinDonationBPSSet(_minDonationBPS);
+    }
+}
---
+++ contracts/test/Challenges.t.sol
@@ -0,0 +1,225 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "forge-std/Test.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "./mock/MockERC20.sol";
+import "../src/Challenges.sol";
+import "../src/interfaces/IChallenges.sol";
+
+contract ChallengesTest is Test {
+    MockERC20 stakingAsset;
+    Challenges challenges;
+    address governance;
+    uint128 initMinDonationBPS = 1_000;
+    address treasury;
+    address verifier;
+    uint256 key;
+    uint64 joinDueDays;
+    uint64 challengeEndDays;
+    uint128 constant PER_USER_STAKE = 100 * 1e6;
+
+    function setUp() public {
+        challengeEndDays = 100;
+        joinDueDays = 1;
+
+        (verifier, key) = makeAddrAndKey("test_verifier");
+        treasury = makeAddr("test_treasury");
+        governance = makeAddr("test_governance");
+
+        stakingAsset = new MockERC20("USDC", "USDC", 6);
+        challenges = new Challenges("Alibuda Habit Builder", "1.0", governance, initMinDonationBPS);
+
+        stakingAsset.mint(address(this), PER_USER_STAKE);
+        stakingAsset.approve(address(challenges), PER_USER_STAKE);
+    }
+
+    function _expectRevertNonOwner(address _sender) internal {
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, _sender));
+    }
+
+    function _createChallenge() internal {
+        vm.prank(governance);
+        challenges.setDonationOrgEnabled(treasury, true);
+        challenges.create(
+            IChallenges.Challenge(
+                verifier,
+                1,
+                uint64(block.timestamp),
+                uint64(block.timestamp + joinDueDays * 1 days),
+                uint64(block.timestamp + challengeEndDays * 1 days),
+                treasury,
+                address(0),
+                address(stakingAsset),
+                6_000,
+                PER_USER_STAKE
+            )
+        );
+    }
+
+    function _joinAndCheckIn(bytes calldata checkInData) internal {
+        vm.warp(block.timestamp);
+
+        challenges.join(1);
+
+        bytes32 digest = challenges.getCheckInDigest(1, checkInData);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(key, digest);
+
+        challenges.checkIn(1, checkInData, abi.encodePacked(r, s, v));
+    }
+
+    function test_CreateChallenge() public {
+        _createChallenge();
+        IChallenges.Challenge memory _challenge = challenges.getChallenge(1);
+        assertNotEq(_challenge.startTimestamp, 0, "Create failed");
+    }
+
+    function test_Join() public {
+        _createChallenge();
+        challenges.join(1);
+        assertEq(
+            uint256(challenges.userStatus(1, address(this))), uint256(IChallenges.UserStatus.Joined), "Join failed"
+        );
+    }
+
+    function test_RevertWhen_JoinPeriodEnded() public {
+        _createChallenge();
+        vm.warp(block.timestamp + 2 days);
+        vm.expectRevert(IChallenges.InvalidTimestamp.selector);
+        challenges.join(1);
+    }
+
+    function test_CheckIn(bytes calldata checkInData) public {
+        _createChallenge();
+        _joinAndCheckIn(checkInData);
+        assertEq(challenges.checkIns(1, address(this), 0), checkInData, "Check in failed");
+    }
+
+    function test_Settle(bytes calldata checkInData) public {
+        _createChallenge();
+        _joinAndCheckIn(checkInData);
+
+        vm.warp(block.timestamp + (challengeEndDays + 1) * 1 days);
+        challenges.settle(1);
+
+        assertEq(uint256(challenges.challengeStatus(1)), uint256(IChallenges.ChallengeStatus.Settled), "Settle failed");
+        assertEq(challenges.getWinningStakePerUser(1), PER_USER_STAKE, "Settle failed");
+    }
+
+    function test_Settle_WhenAllUserFailed() public {
+        _createChallenge();
+        challenges.join(1);
+
+        vm.warp(block.timestamp + (challengeEndDays + 1) * 1 days);
+        challenges.settle(1);
+
+        assertEq(uint256(challenges.challengeStatus(1)), uint256(IChallenges.ChallengeStatus.Settled), "Settle failed");
+        assertEq(challenges.getWinningStakePerUser(1), 0, "Settle failed");
+        assertEq(stakingAsset.balanceOf(address(treasury)), PER_USER_STAKE, "fund left in contract");
+    }
+
+    function test_Withdraw(address failedUser, bytes calldata checkInData) public {
+        vm.assume(failedUser != address(0));
+        _createChallenge();
+        _joinAndCheckIn(checkInData);
+
+        vm.startPrank(failedUser);
+        stakingAsset.mint(failedUser, PER_USER_STAKE);
+        stakingAsset.approve(address(challenges), PER_USER_STAKE);
+        challenges.join(1);
+        vm.stopPrank();
+
+        vm.warp(block.timestamp + (challengeEndDays + 1) * 1 days);
+        challenges.settle(1);
+
+        uint256 beforeBalance = stakingAsset.balanceOf(address(this));
+        challenges.withdraw(1);
+        uint256 afterBalance = stakingAsset.balanceOf(address(this));
+        IChallenges.Challenge memory _challenge = challenges.getChallenge(1);
+        assertEq(
+            afterBalance - beforeBalance,
+            PER_USER_STAKE + PER_USER_STAKE * (10_000 - _challenge.donationBPS) / 10_000,
+            "testing withdraw failed"
+        );
+        assertEq(stakingAsset.balanceOf(treasury), PER_USER_STAKE * _challenge.donationBPS / 10_000, "donation failed");
+        assertEq(stakingAsset.balanceOf(address(challenges)), 0, "fund left in contract");
+    }
+
+    function test_FailedUserWithdraw() public {
+        _createChallenge();
+        challenges.join(1);
+
+        vm.warp(block.timestamp + (challengeEndDays + 1) * 1 days);
+        challenges.settle(1);
+
+        vm.expectRevert(IChallenges.UserNotClaimable.selector);
+        challenges.withdraw(1);
+    }
+
+    function test_UpdateDonationOrgStatus(address donationOrg) public {
+        vm.assume(donationOrg != address(0));
+        vm.startPrank(governance);
+        challenges.setDonationOrgEnabled(donationOrg, true);
+        assertEq(challenges.donationOrgsEnabled(donationOrg), true, "updateDonationOrgStatus failed");
+    }
+
+    function test_RevertWhen_UpdateDonationOrgStatus_WithInvalidPermission(address donationOrg) public {
+        _expectRevertNonOwner(address(this));
+        challenges.setDonationOrgEnabled(donationOrg, true);
+    }
+
+    function test_RevertWhen_DonationOrgDisabled(address donationOrg) public {
+        vm.assume(donationOrg != address(0));
+        vm.expectRevert(IChallenges.InvalidPermission.selector);
+        challenges.create(
+            IChallenges.Challenge(
+                verifier,
+                1,
+                uint64(block.timestamp),
+                uint64(block.timestamp + joinDueDays * 1 days),
+                uint64(block.timestamp + challengeEndDays * 1 days),
+                donationOrg,
+                address(0),
+                address(stakingAsset),
+                0,
+                PER_USER_STAKE
+            )
+        );
+    }
+
+    function test_setMinDonationBPS(uint128 minDonationBPS) public {
+        vm.assume(minDonationBPS < challenges.MAX_BPS());
+        vm.startPrank(governance);
+        challenges.setMinDonationBPS(minDonationBPS);
+        assertEq(challenges.minDonationBPS(), minDonationBPS, "setMinDonationBPS failed");
+    }
+
+    function test_RevertWhen_SetMinDonationBP_WithInvalidPermission(uint128 minDonationBPS) public {
+        _expectRevertNonOwner(address(this));
+        challenges.setMinDonationBPS(minDonationBPS);
+    }
+
+    function test_RevertWhen_CreateChallenge_WithBPSTooLow(uint128 minDonationBPS, uint128 donationBPS) public {
+        vm.assume(minDonationBPS < challenges.MAX_BPS() && minDonationBPS > donationBPS);
+        vm.startPrank(governance);
+        challenges.setDonationOrgEnabled(treasury, true);
+        challenges.setMinDonationBPS(minDonationBPS);
+        vm.expectRevert(IChallenges.InvalidBPS.selector);
+        challenges.create(
+            IChallenges.Challenge(
+                verifier,
+                1,
+                uint64(block.timestamp),
+                uint64(block.timestamp + joinDueDays * 1 days),
+                uint64(block.timestamp + challengeEndDays * 1 days),
+                treasury,
+                address(0),
+                address(stakingAsset),
+                donationBPS,
+                PER_USER_STAKE
+            )
+        );
+    }
+}
