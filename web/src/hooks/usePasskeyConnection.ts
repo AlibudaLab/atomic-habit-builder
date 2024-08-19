8c8630ffc8b56e2e6d068db191c0bd6204356e5b
Anton Cheng <antonassocareer@gmail.com>
2024-08-20 00:25:49 +0800

    chore: add initalizing in login button, avoid changing of text while reading usedPassKeyBefore

--- web/src/hooks/usePasskeyConnection.ts
+++ web/src/hooks/usePasskeyConnection.ts
@@ -1,18 +1,30 @@
 'use client';
 
 import storage from 'local-storage-fallback';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useConnect } from 'wagmi';
 
 export default function usePasskeyConnection() {
   const { connectors, connect, isPending } = useConnect();
+  const [initializing, setInitializing] = useState(true);
+  const [signedInBefore, setSignedInBefore] = useState(false);
 
-  const signedInBefore = useMemo(() => storage.getItem('zerodev_wallet_signer') !== null, []);
+  useEffect(() => {
+    const usedBefore = storage.getItem('zerodev_wallet_signer') !== null;
+    setSignedInBefore(usedBefore);
+    setInitializing(false);
+  }, []);
 
   return {
+    initializing,
     signedInBefore,
     register: () => connect({ connector: connectors[0] }),
-    login: () => connect({ connector: connectors[1] }),
+    login: () => {
+      connect(
+        { connector: connectors[1] },
+        { onError: (e) => console.log('login error', e.message) },
+      );
+    },
     isPending,
   };
 }
