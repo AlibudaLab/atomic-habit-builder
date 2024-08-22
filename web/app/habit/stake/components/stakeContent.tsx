b20e3be0fed0cdb028cc455d5d64719cc8d8355e
Anton Cheng <antonassocareer@gmail.com>
2024-08-22 21:34:25 +0800

    fix: refetch

--- web/app/habit/stake/components/stakeContent.tsx
+++ web/app/habit/stake/components/stakeContent.tsx
@@ -24,6 +24,7 @@ import { Button } from '@nextui-org/button';
 import useUserStatus from '@/hooks/useUserStatus';
 import { Environment, getCurrentEnvironment } from '@/store/environment';
 import usePasskeyConnection from '@/hooks/usePasskeyConnection';
+import { useAllChallenges } from '@/providers/ChallengesProvider';
 
 const isTestnet = getCurrentEnvironment() === Environment.testnet;
 
@@ -40,6 +41,8 @@ export default function StakeChallenge() {
   const [inputAccessCode, setInputAccessCode] = useState<string>(attachedCode);
 
   const { challenge, loading: loadingChallenge } = useChallenge(Number(challengeId));
+
+  const { refetch: refetchAllChallenges } = useAllChallenges()
 
   const { address: smartWallet } = useAccount();
   const { joined, refetch } = useUserStatus(smartWallet, Number(challengeId));
@@ -107,7 +110,8 @@ export default function StakeChallenge() {
     isPreparing: isJoinPreparing,
     isLoading: isJoinLoading,
   } = useJoinChallenge(address, BigInt(challenge?.id ?? 0), challenge?.stake ?? BigInt(0), () => {
-    refetch().catch((e) => console.log('refetch error', e))
+    Promise.all([refetch(), refetchAllChallenges()]).catch((e) => console.log('refetch error', e))
+
     handleOpenCheckinPopup(); // trigger pop up window
   });
 
