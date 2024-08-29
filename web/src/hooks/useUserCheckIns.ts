14ef68a088442ad1e98c6ce7c8ece070fecf0642
ryanycw <ryan@ryanycw.dev>
2024-08-29 19:05:43 +0800

    build: update all read contract usage to api

--- web/src/hooks/useUserCheckIns.ts
+++ web/src/hooks/useUserCheckIns.ts
@@ -1,24 +1,38 @@
-import { abi } from '@/abis/challenge';
-import { useMemo } from 'react';
-import { challengeAddr } from '@/constants';
-import { useReadContract } from 'wagmi';
+import { useMemo, useState, useEffect, useCallback } from 'react';
 
 const useUserCheckIns = (address: string | undefined, challengeId: number) => {
-  const {
-    data,
-    status: queryStatus,
-    refetch,
-  } = useReadContract({
-    abi,
-    address: challengeAddr,
-    functionName: 'getUserCheckInCounts',
-    args: [BigInt(challengeId), address as `0x${string}`],
-  });
+  const [counter, setCounter] = useState(0);
+  const [checkIns, setCheckIns] = useState<number | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
 
-  const loading = useMemo(() => queryStatus === 'pending', [queryStatus]);
-  const error = useMemo(() => queryStatus === 'error', [queryStatus]);
+  useEffect(() => {
+    const fetchStatus = async () => {
+      if (!address) return;
+      setLoading(true);
+      try {
+        console.log('fetch userCheckIns counter', counter);
+        const response = await fetch(`/api/on-chain/${address}/${challengeId}/checkins`, {
+          next: { revalidate: 0 },
+        });
+        if (!response.ok) throw new Error('Failed to fetch user status');
+        const data = await response.json();
+        setCheckIns(data.checkInCount);
+      } catch (err) {
+        setError(err instanceof Error ? err : new Error('An error occurred'));
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  const checkedIn = useMemo(() => (data === undefined ? 0 : Number(data)), [data]);
+    fetchStatus().catch(console.error);
+  }, [address, challengeId, counter]);
+
+  const checkedIn = useMemo(() => (checkIns === undefined ? 0 : checkIns), [checkIns]);
+
+  const refetch = useCallback(() => {
+    setCounter((c) => c + 1);
+  }, []);
 
   return { loading, checkedIn, error, refetch };
 };
