9bfe4d70769a8261cabb14e25c616ad9319d7277
Anderson Chen <twan0817@gmail.com>
2024-09-04 17:13:27 +0800

    test: fix openApp test

--- web/cypress/e2e/openApp.cy.ts
+++ web/cypress/e2e/openApp.cy.ts
@@ -4,7 +4,7 @@ describe('Open the app', () => {
     })
   
     it('should display the onboard page', () => {
-      cy.get('div').should('contain', 'Welcome to Atomic')
+      cy.get('p').should('contain', 'Stake to join a challenge.')
       cy.get('button').should('contain', 'Sign in')
     })
 
@@ -15,12 +15,16 @@ describe('Open the app', () => {
 
     it('should able to enter a challenge page', () => {
         cy.visit('http://localhost:3000/habit/list')
-        cy.get(':nth-child(3) > .wrapped > .w-full').click()
-        cy.get('div').should('contain', 'Stake and Commit to It!')
+        cy.get('button').contains('Let’s Cycling Challenge 🚴').click()
+
+        // Check if the challenge details are displayed
+        cy.get('div').should('contain', 'Goal')
+        cy.get('div').should('contain', 'Check In')
+        cy.get('div').should('contain', 'Stake Amount')
         cy.get('button').should('contain', 'Join This Challenge')
 
         // Click on join button
-        cy.get('.z-0').click()
+        cy.get('button').contains('Join This Challenge').click()
         // Check if the modal id open
         cy.get('[role="dialog"]').should('be.visible')
         // Check if the button text is "Sign in"
@@ -33,9 +37,10 @@ describe('Open the app', () => {
         
         // Fill in the form
         cy.contains('label', 'Challenge Name').should('exist')
-        cy.get('.px-8 > :nth-child(2) > .relative > .inline-flex').type('Test Challenge')
-        cy.get(':nth-child(3) > .tap-highlight-transparent').type('Test Description')
-        // cy.get('input[label="Challenge Name"]').type('Test Challenge')
+        // assume the second input is the challenge name
+        cy.get('input').eq(1).type('Test Challenge')
+        // assume the first textarea is the challenge description
+        cy.get('textarea').eq(0).type('Test Description')
         
         // Click on the next button
         cy.get('button').contains('Next').click()
