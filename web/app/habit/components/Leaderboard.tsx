2ebe49e461a488b96bc7052bf2b50737f413defc
Anderson Chen <twan0817@gmail.com>
2024-09-07 00:47:01 +0800

    style: make the leaderboard be like nicole's desgin

--- web/app/habit/components/Leaderboard.tsx
+++ web/app/habit/components/Leaderboard.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from '@nextui-org/react';
 import { User } from '@/types';
+import { ChallengeTypes } from '@/constants';
 
 type LeaderboardProps = {
   userRankings: User[];
   address: string;
   challenge: {
     minimumCheckIns: number;
+    type: ChallengeTypes;
   };
 }
 
 function Leaderboard({ userRankings, address, challenge }: LeaderboardProps) {
   return (
     <div className="w-full max-w-md mt-8">
-      <div className="text-xl text-dark mb-5">Leaderboard</div>
+      <div className="text-xl text-dark mb-5">🏆Leaderboard</div> 
       <Table aria-label="Leaderboard">
         <TableHeader>
           <TableColumn className="text-center bg-white">Rank</TableColumn>
-          <TableColumn className="text-center bg-white">Address</TableColumn>
+          <TableColumn className="text-center bg-white">ID</TableColumn>
           <TableColumn className="text-center bg-white">Check-ins</TableColumn>
         </TableHeader>
         <TableBody>
@@ -29,16 +31,25 @@ function Leaderboard({ userRankings, address, challenge }: LeaderboardProps) {
                 index === 2 ? 'text-amber-600' :
                 ''
               }`}>
-                {index + 1}
+                No. {index + 1}
               </TableCell>
-              <TableCell className={`font-bold ${
-                user.id === address ? "bg-primary text-white rounded-lg" : ""
-              }`}>
-                {user.id === address
-                  ? "You"
-                  : `${user.id.slice(0, 4)}...${user.id.slice(-4)}`}
+              <TableCell>
+                <div
+                  id="super-good-ball"
+                  className={`font-bold flex justify-center items-center ${
+                    user.id === address.toLocaleLowerCase() ? "bg-primary text-white" : "bg-dark text-white"
+                  }`}
+                  style={{
+                    borderRadius: '50%',
+                    height: '60px',
+                  }}
+                  >
+                  {user.id === address.toLocaleLowerCase()
+                    ? "You"
+                    : `${user.id.slice(2, 5)}`}
+                </div>
               </TableCell>
-              <TableCell className='font-bold'>{user.totalCheckIns} / {challenge.minimumCheckIns}</TableCell>
+              <TableCell className='font-bold'>{user.totalCheckIns} {challenge.type}s</TableCell>
             </TableRow>
           ))}
         </TableBody>
