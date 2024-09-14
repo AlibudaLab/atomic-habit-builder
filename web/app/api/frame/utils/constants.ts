604b3809649ad02b6398cf34fda8fa3cb1e30bc2
ryanycw <ryan@ryanycw.dev>
2024-09-15 01:02:26 +0800

    chore: fix key name

--- web/app/api/frame/utils/constants.ts
+++ web/app/api/frame/utils/constants.ts
@@ -1,13 +1,13 @@
 export const gifUrls = {
-  cycling: [
+  Cycling: [
     'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYTY4bzdpcDQ0Y3RycnFxNGE3eTd2ZzQ2NnRhNmVmamd3ZDFqcHB3bSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Xs7tigYjWwhkSFR2Ih/giphy.gif',
     'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHc0bnU2eGlvbHRuOHBtZG54aW9uNHNoZ212ZmNiNTZxMnM4bXozdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3owvJVUhntFJjT4usg/giphy.gif',
   ],
-  run: [
+  Run: [
     'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExeG56bTFnNm14cnJoNHViZzZ6ODlrYnBrejZqejl6OTVyZW9iZnc5aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7ZetIsjtbkgNE1I4/giphy.gif',
     'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGpucGM0aW10OGprZGxxY2k3NTlmdmtsc3NlOW1oa2w1MWg4cTcybCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/KFNcRk0ct77s4/giphy.gif',
   ],
-  workout: [
+  Workout: [
     'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXRqbHI5cjJnbnM1eXNlejZnaXI1Njlsc2FiOG4zMHQ3Mm9xdzZwMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1qXUUWZ1We4zC/giphy.gif',
     'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbDQyNXF5bWQydzl5bmp3MW5wb2gwYWFndHFqZW9jZXUwcDFmYXVjdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/5EJHDSPpFhbG0/giphy.gif',
   ],
