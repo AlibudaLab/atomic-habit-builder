343bde6542e00897d4b5f3e79dfe1e29d73e98e2
Anton Cheng <antonassocareer@gmail.com>
2024-09-15 12:03:09 +0800

    chore: revert unused changes

--- web/app/api/strava/[activityType]/route.ts
+++ web/app/api/strava/[activityType]/route.ts
@@ -36,21 +36,7 @@ export async function GET(
       activityTypes.includes(activity.type),
     );
 
-    if (activityTypeKey === 'all') {
-      return NextResponse.json(
-        {
-          activities: activities.map((activity: any) => {
-            return {
-              id: activity.id,
-              name: activity.name,
-              timestamp: activity.start_date,
-              moving_time: activity.moving_time,
-            };
-          }),
-        },
-        { status: 200 },
-      );
-    } else if (activityTypeKey === 'workout') {
+    if (activityTypeKey === 'workout') {
       return NextResponse.json(
         {
           activities: activities.map((activity: any) => {
@@ -59,9 +45,6 @@ export async function GET(
               name: activity.name,
               moving_time: activity.moving_time,
               timestamp: activity.start_date,
-              distance: activity.distance,
-              map: activity.map,
-              max_heartrate: activity.max_heartrate,
             };
           }),
         },
