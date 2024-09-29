c23d90071d5101fed2c6b800a5b05498d958982d
Anton Cheng <antonassocareer@gmail.com>
2024-09-29 22:35:43 +0800

    chore: fix no-onging treated as new user

--- web/app/habit/components/UserDashboard.tsx
+++ web/app/habit/components/UserDashboard.tsx
@@ -6,24 +6,37 @@ import { ChallengePreview } from './ChallengeBox';
 import { useRouter } from 'next/navigation';
 import NewUser from './NewUser';
 import { logEventSimple } from '@/utils/gtag';
+import { Button } from '@nextui-org/button';
 
 type DashboardProps = {
   onGoingChallenges: ChallengeWithCheckIns[];
+  totalChallengeCount: number;
 };
 
-export default function Dashboard({ onGoingChallenges }: DashboardProps) {
+export default function Dashboard({ onGoingChallenges, totalChallengeCount }: DashboardProps) {
   const { push } = useRouter();
 
-  return onGoingChallenges.length === 0 ? (
+  return totalChallengeCount === 0 ? (
     <NewUser />
   ) : (
     <div className="flex h-screen w-full flex-col items-center justify-start">
       {/* if no challenges, show new user component */}
       <div className="flex w-full items-center justify-center">
-        {onGoingChallenges.length > 0 && (
-          <p className="my-4 font-londrina text-xl font-bold"> My Challenges </p>
-        )}
+        <p className="my-4 font-londrina text-xl font-bold"> My Challenges </p>
       </div>
+
+      {onGoingChallenges.length == 0 && (
+        <div className="flex flex-col items-center justify-center">
+          <p className="m-4 font-nunito text-base">No on going challenges, try joining one!</p>
+          <Button
+            color="primary"
+            className="mt-4 w-1/2 no-underline"
+            onClick={() => push('/habit/list')}
+          >
+            <div className="my-4 rounded-lg p-4">Explore</div>
+          </Button>
+        </div>
+      )}
 
       {/* map challenges to list of buttons */}
       {onGoingChallenges.map((challenge, idx) => (
