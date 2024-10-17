511090f130fad19b105f7803d856cc8a8c878bf9
ryanycw <ryan@ryanycw.dev>
2024-10-18 01:50:51 +0800

    chore: adjust qrcode styling

--- web/app/profile/components/QRCodeDialog.tsx
+++ web/app/profile/components/QRCodeDialog.tsx
@@ -42,7 +42,7 @@ export function QRCodeDialog({
               </>
             )}
           </ModalBody>
-          <ModalFooter>
+          <ModalFooter className="flex justify-center">
             {!isError && (
               <Button
                 color="primary"
@@ -54,7 +54,7 @@ export function QRCodeDialog({
                 <svg xmlns="http://www.w3.org/2000/svg" width={12} height={18} fill="none">
                   <title>Sign in With Farcaster QR Code</title>
                   <path
-                    fill="#7C65C1"
+                    fill="currentColor"
                     fillRule="evenodd"
                     d="M0 3a3 3 0 0 1 3-3h6a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V3Zm4-1.5v.75c0 .414.336.75.75.75h2.5A.75.75 0 0 0 8 2.25V1.5h1A1.5 1.5 0 0 1 10.5 3v12A1.5 1.5 0 0 1 9 16.5H3A1.5 1.5 0 0 1 1.5 15V3A1.5 1.5 0 0 1 3 1.5h1Z"
                     clipRule="evenodd"
